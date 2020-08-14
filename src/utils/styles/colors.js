b152f2667da6d0aa59f73d8683ea2dd8ebb4a0f4
Mohammadreza Salari <frishter.ms@gmail.com>
2020-08-14 20:47:00 +0430

    update colors

---
+++ src/utils/styles/colors.js
@@ -0,0 +1,16 @@
+const colors = {
+  appColor: "#243C6C",
+  white: "#FFFFFF",
+  black: "#000000",
+  nero: "#262626",
+  primaryButtonColor: "#FFBB00",
+};
+
+export default {
+  primaryColor: colors.appColor,
+  primaryBackground: colors.white,
+  primaryText: colors.black,
+  secondaryText: colors.nero,
+  primaryButtonBackground: colors.primaryButtonColor,
+  primaryButtonText: colors.white,
+};
