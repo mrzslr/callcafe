dbed2a038f50a66cb9c3943695b7f4877cb4b896
Mohammadreza Salari <frishter.ms@gmail.com>
2020-08-15 20:23:48 +0430

    update CategoryItem component.

--- src/components/CategoryItem/index.js
+++ src/components/CategoryItem/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import styles from './style';
-import injectSheet from 'react-jss';
-import mugCup from 'assets/img/mug.svg';
+import React from "react";
+import styles from "./style";
+import injectSheet from "react-jss";
 
-const CategoryItem  = ({classes})=>{
-    return <div align="center" className={classes.container}>
-        <img src={mugCup} className={classes.icon}/>
+const CategoryItem = ({ classes, category }) => {
+  return (
+    <div align="center" className={classes.container}>
+      <img src={category.image} className={classes.icon} alt={category.title} />
     </div>
-}
-export default injectSheet(styles)(CategoryItem)
+  );
+};
+export default injectSheet(styles)(CategoryItem);
