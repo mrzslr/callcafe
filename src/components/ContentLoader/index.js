1f000e1ba105c1772cd727777870665fe52d951c
Mohammadreza Salari <frishter.ms@gmail.com>
2020-08-15 20:23:13 +0430

    update ContentLoader component

--- src/components/ContentLoader/index.js
+++ src/components/ContentLoader/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import styles from './style';
-import injectSheet from 'react-jss';
-import imgLatte from 'assets/img/latte.png'
+import React from "react";
+import styles from "./style";
+import injectSheet from "react-jss";
+import imgLatte from "assets/img/latte.png";
+import Slider from "components/Slider";
 
-const ContentLoader =({classes})=>{
-    return <div align="center" className={classes.container}>
-        <img src={imgLatte} />
+const ContentLoader = ({ classes }) => {
+  return (
+    <div align="center" className={classes.container}>
+      
+      <Slider
+        options={{
+          wrapAround: true,
+          prevNextButtons: false,
+          pageDots: false,
+        }}
+      >
+        <div className={classes.carouselCell}>
+          {" "}
+          <div className={classes.cover}>
+            <img src={imgLatte} />
+            <div align="center" className={classes.waveBg}></div>
+          </div>
+        </div>
+        <div className={classes.carouselCell}></div>
+        <div className={classes.carouselCell}></div>
+        <div className={classes.carouselCell}></div>
+        <div className={classes.carouselCell}></div>
+      </Slider>
     </div>
-}
-export default injectSheet(styles)(ContentLoader)
+  );
+};
+export default injectSheet(styles)(ContentLoader);
