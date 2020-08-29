02c35799c76b7afaf6cd32e8a4bfae80faa1c51c
Mohammadreza Salari <frishter.ms@gmail.com>
2020-08-29 18:50:17 +0430

    create listeners and correct Home component

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ContentLoader from "components/ContentLoader";
-import CategoryMenu from 'components/CategoryMenu'
+import CategoryMenu from "components/CategoryMenu";
+import menu from "data/menu";
 const Home = () => {
+  const [activeCategory, setActiveCategory] = useState(menu[0].key);
+
+  const onCategoryMenuChanged = (activeKey) => {
+    console.log(activeKey)
+    setActiveCategory(activeKey);
+  };
+
   return (
     <div>
-      <CategoryMenu />
       <ContentLoader />
+
+      <CategoryMenu
+        menu={menu}
+        activeCategory={activeCategory}
+        onCategoryChanged={onCategoryMenuChanged}
+      />
     </div>
   );
 };
