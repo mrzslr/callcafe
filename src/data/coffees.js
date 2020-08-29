6fc8014be0776651c6b4a270c5e8af826c4f6069
Mohammadreza Salari <frishter.ms@gmail.com>
2020-08-29 18:49:52 +0430

    add mock data for menu and coffees

---
+++ src/data/coffees.js
@@ -0,0 +1,40 @@
+export const coffees = [
+  {
+    id: 1,
+    title: "اسپرسو",
+    key: "espresso",
+    image: "/hotcoffee/1.png",
+  },
+  {
+    id: 2,
+    title: "موکا",
+    key: "moca",
+    image: "/hotcoffee/2.png",
+  },
+  {
+    id: 3,
+    title: "لاته",
+    key: "latte",
+    image: "/hotcoffee/3.png",
+  },
+  {
+    id: 4,
+    title: "آمریکانو",
+    key: "americano",
+    image: "/hotcoffee/4.png",
+  },
+  {
+    id: 5,
+    title: "کاپوچینو",
+    key: "cappoccino",
+    image: "/hotcoffee/5.png",
+  },
+  {
+    id: 5,
+    title: "آفوگاتو",
+    key: "affogato",
+    image: "/hotcoffee/6.png",
+  },
+];
+
+export default coffees;
---
+++ src/data/menu.js
@@ -0,0 +1,28 @@
+export const menu = [
+  {
+    id: 1,
+    title: "بار گرم",
+    key: "hot",
+    image: "/mug.svg",
+  },
+  {
+    id: 2,
+    title: "بار سرد",
+    key: "cold",
+    image: "/coldbrew.svg",
+  },
+  {
+    id: 3,
+    title: "غذا",
+    key: "food",
+    image: "/food.svg",
+  },
+  {
+    id: 4,
+    title: "کیک و کوکی",
+    key: "cakes",
+    image: "/cake.svg",
+  },
+];
+
+export default menu;
